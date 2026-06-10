6176863274e69b54d65a88cad35464f48382e852
kardapreet3-cmd <kardapreet3@gmail.com>
2026-06-10 12:27:13 +0530

    DAY-03

---
+++ accessing_elements_list.py
@@ -0,0 +1,12 @@
+fruits = ["apple","banana","orange","mango"]
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+#negative indexing
+print(fruits[-1])
+print(fruits[-2])
+print(fruits[-3])
+print(fruits[-4])
+#slicing
+print(fruits[0:2])
