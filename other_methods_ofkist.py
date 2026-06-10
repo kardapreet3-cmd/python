8e97be402430b364bdcdea654efc018375c5a255
kardapreet3-cmd <kardapreet3@gmail.com>
2026-06-10 12:41:04 +0530

    DAY-03

---
+++ other_methods_ofkist.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "dryganfruit","cherry"]
+print(fruits)
+print(len(fruits))
+print(fruits.sort())
+print(fruits.reverse())
+print(fruits.copy())
+print(fruits.index("apple"))
+print(fruits.count("apple"))
