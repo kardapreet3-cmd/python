a213d258386005d247bbfb2b5fffca6a187d1da5
kardapreet3-cmd <kardapreet3@gmail.com>
2026-06-10 12:39:22 +0530

    Day-03

---
+++ adding_element_list.py
@@ -0,0 +1,11 @@
+#append
+fruits = ["apple","banana","mango"]
+fruits.append("grape")
+print(fruits)
+#insert
+fruits.insert(1,"chiku")
+print(fruits)
+#extend
+list=[1,2,3,4,5]
+fruits.extend(list)
+print(fruits)
