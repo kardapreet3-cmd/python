70f800e3bcfd911d0f0fd3c28319366da4132ce0
kardapreet3-cmd <kardapreet3@gmail.com>
2026-06-10 12:40:17 +0530

    Day-03

---
+++ modifying_elements_list.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+fruits[1] = "orange"
+print(fruits)
+fruits[1:3]=["kiwi", "melon"]
+print(fruits)
+fruits.insert(1,"grape")
+fruits.extend(["apple"])
+print(fruits)
