31735cd2cc6a57d8e9e0e5d1da126901b5451335
kardapreet3-cmd <kardapreet3@gmail.com>
2026-06-10 12:53:21 +0530

    Day-03

---
+++ sets.py
@@ -0,0 +1,15 @@
+from list import empty
+
+nums={1,2,3,4,5,6,7,8,9}
+fruits ={"apple","banana","mango"}
+print(nums)
+print(fruits)
+#duplicates remove
+nums={1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8}
+print(nums)
+#list in sets
+emails=['a@g.com','b@g.com']
+unique=list(set(emails))
+print(unique)
+empty={}
+print(empty)
