cd3a8dce08d73f8371801df14bb2d4e39a81c794
kardapreet3-cmd <kardapreet3@gmail.com>
2026-06-12 12:47:40 +0530

    Day-05

---
+++ assig[A].py
@@ -0,0 +1,15 @@
+#Q1:TAKE AND DISPLAY DATA OF EMP
+emp_id=int(input('Enter emp id: '))
+emp_name=str(input('Enter emp name: '))
+emp_email=str(input('Enter emp email: '))
+emp_contact=(input('Enter emp contact: '))
+emp_address=str(input('Enter emp address: '))
+emp_department=str(input('Enter emp department: '))
+print("Employee ID: ", emp_id)
+print("Employee Name: ", emp_name)
+print("Employee Email: ", emp_email)
+print("Employee Contact: ", emp_contact)
+print("Employee Address: ", emp_address)
+print("Employee Department: ", emp_department)
+
+
