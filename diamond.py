4716876220ef37c009c12ebe8bfd502997cd1f2f
kardapreet3-cmd <kardapreet3@gmail.com>
2026-06-17 11:08:50 +0530

    Day-06

---
+++ diamond.py
@@ -0,0 +1,23 @@
+for i in range(1,6):
+    print(" "*(5-i)+"* "*i)
+for j in range(5-1,0,-1):
+    print(" "*(5-j)+"* "*j)
+
+#printing spaces
+#5-0-1=4
+#5-1-1=3
+#5-2-1=2
+#5-3-1=1
+#5-4-1=0
+#printing *
+#2*0+1=1
+#2*1+1=3
+#2*2+1=5
+#2*3+1=7
+#2*4+1=9
+# rows=5
+# for i in range(rows):
+#     print(" "*(rows-i-1 )+ "*"*(2*i=1))
+#     if i==5:
+#         for j in range(rows-i-1):
+#             print(" "*(rows))print
